62c44cbb52b80485f184d6b7cd59690ae6c01a62
Daniel Toro <daniel.toro.rod@gmail.com>
2021-04-23 19:50:23 -0400

    integración visual de back y front

--- front/assets/js/codigo.js
+++ front/assets/js/codigo.js
@@ -1,8 +1,12 @@
-// console.log(window);
+console.log(window.location.search);
+var busqueda = window.location.search || "";
 
-fetch("http://localhost:3600/search",{method: 'GET',headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-}}).then(response=>response.json())
+fetch("http://localhost:3600/search"+ busqueda, {
+    method: 'GET',
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    }
+}).then(response=>response.json())
 .then(json=>agrupar(json))
 .then(json=>cargarProductos(json));
 
@@ -34,9 +38,13 @@ function cargarProductos(json) {
     Object.keys(json).forEach(v=>{
         let productos = document.querySelector("#"+v.replace(" ","-")+" .row");
         let productoHTML = "";
-        json[v].forEach(p=>{productoHTML += `
-        <div class="col-3">
-            <p>lorem ipsum moro ccss ana ana ano</p>
+        json[v].forEach(p=>{
+            productoHTML += `
+        <div class="col-12 col-lg-3">
+            <img class="w-100" src="${p.url_image || "./assets/img/carro_compra.webp"}"></img>
+            <p class="text-uppercase">${p.product}</p>
+            <p>$${p.price} pesos</p>
+            <p>${p.discount}% de descuento</p>
         </div>
         `;});
         productos.innerHTML = productoHTML;
